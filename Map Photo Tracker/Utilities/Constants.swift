ca718973152b8be09c51569d7fb858789056337e
Milan Bojic <milan.bojic.developer@gmail.com>
2018-11-14 16:11:02 +0100

    Flickr apiKey and url added

---
+++ Map Photo Tracker/Utilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Map Photo Tracker
+//
+//  Created by Milan Bojic on 11/14/18.
+//  Copyright © 2018 Milan Bojic. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "957767c9c7566ae9b4381e404230828a"
+
+func flickrUrl(forApiKey apiKey: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos number: Int) -> String {
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=km&per_page=\(number)&format=json&nojsoncallback=1"
+}
